2b5c8f9b3b00b4220f3b4083561f62218f5216f8
Eric Grunzke <eric@grunzke.com>
2018-03-02 14:58:24 -0700

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='arctic_tern',
+    version='0.1.0',
+    packages=['arctic_tern'],
+    url='https://github.com/ConvergysLabs/arctic-tern',
+    license='MIT',
+    author='Eric Grunzke',
+    author_email='eric@grunzke.com',
+    description='SQL migrations for python and postgres'
+)
