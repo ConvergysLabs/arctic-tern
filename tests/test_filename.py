236fb5b2a5f66f18fe740efc1d8a4d75ee7cd74c
Eric Grunzke <eric@grunzke.com>
2018-04-27 11:38:02 -0600

    Add posix hashes for testing

--- tests/test_filename.py
+++ tests/test_filename.py
@@ -26,16 +26,15 @@ def test_fails(test_filename):
     assert val is None
 
 
-@pytest.mark.parametrize("file_, stamp, label, hash_", [
-    ("1-person-address.sql", 1, "person-address", "be51b1ac168191cb6d3741c717ed71492f5a913b144170f1ca9fed82"),
-    ("2-add-apartment.sql", 2, "add-apartment", "714ac7e2ae171eddcb0687fd5fc81af95d83b9e49c8c48edc3d51be0"),
-    ("3-fail.sql", 3, "fail", "1ad019a8d4e278d5265b05ca19d9a6d17259f973a0ad053d3b4f2a19"),
+@pytest.mark.parametrize("file_, stamp, label, win_hash, posix_hash", [
+    ("1-person-address.sql", 1, "person-address", "be51b1ac168191cb6d3741c717ed71492f5a913b144170f1ca9fed82", "920366b2c5321fc908b06c86047e30e9b63f39b2b240211645991782"),
+    ("2-add-apartment.sql", 2, "add-apartment", "714ac7e2ae171eddcb0687fd5fc81af95d83b9e49c8c48edc3d51be0", "714ac7e2ae171eddcb0687fd5fc81af95d83b9e49c8c48edc3d51be0"),
+    ("3-fail.sql", 3, "fail", "1ad019a8d4e278d5265b05ca19d9a6d17259f973a0ad053d3b4f2a19", "7b21676def0b49f10b949c194c3cb1cc0c5fb35077d6d3018fe1114b"),
 ])
-def test_mf(file_, stamp, label, hash_):
+def test_mf(file_, stamp, label, win_hash, posix_hash):
     dir_ = "tests/scripts"
     mf = construct_migration(file_, dir_)
     assert mf.stamp == stamp
     assert mf.name == label
     assert os.path.split(mf.path) == (dir_, file_)
-    assert mf.hash_ == hash_
-
+    assert mf.hash_ == win_hash or mf.hash_ == posix_hash
