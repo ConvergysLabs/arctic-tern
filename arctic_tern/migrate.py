b4eaa365374d77a131f290b54a9d3ddacf880759
Eric Grunzke <eric@grunzke.com>
2020-05-19 17:53:43 -0600

    Working with asyncpg

--- arctic_tern/migrate.py
+++ arctic_tern/migrate.py
@@ -1,29 +1,44 @@
 import logging
 import os
+import re
 from typing import List
 
-import psycopg2
-from psycopg2.extensions import connection, cursor
+from asyncpg.connection import Connection
+from asyncpg.exceptions import PostgresError
 
 from arctic_tern.filename import construct_migration, MigrationFile
 
 log = logging.getLogger("arctic_tern.migrate")
+illegal_schema_chars = re.compile(r"[^\w]+")
 
 
-def migrate(migration_dir: str, conn: connection, schema: str = None):
-    _migrate(_get_sql_files(migration_dir), conn, schema)
+async def migrate(migration_dir: str, conn: Connection, schema: str = None):
+    await _set_schema_path(conn, schema)
+    await _migrate(_get_sql_files(migration_dir), conn, schema)
 
 
-def migrate_multi(migration_dirs: List[str], conn: connection, schema: str = None):
+async def migrate_multi(migration_dirs: List[str], conn: Connection, schema: str = None):
+    await _set_schema_path(conn, schema)
     combined_migrations = []
     for md in migration_dirs:
         combined_migrations.extend(_get_sql_files(md))
-    _migrate(combined_migrations, conn, schema)
+    await _migrate(combined_migrations, conn, schema)
 
 
-def _migrate(migrations: List[MigrationFile], conn: connection, schema: str = None):
-    _prepare_meta_table(conn, schema)
-    prev_mig = _fetch_previous_migrations(_get_schema_cursor(conn, schema))
+async def _set_schema_path(conn: Connection, schema: str):
+    if not schema:
+        return
+
+    if illegal_schema_chars.search(schema):
+        raise ValueError("Illegal characters found in schema name")
+
+    sql = f'SET search_path TO "{schema}"'
+    await conn.execute(sql)
+
+
+async def _migrate(migrations: List[MigrationFile], conn: Connection, schema):
+    await _prepare_meta_table(conn, schema)
+    prev_mig = await _fetch_previous_migrations(conn)
     prev_mig_iter = iter(prev_mig)
     current_mig = _next_or_none(prev_mig_iter)
     migrations.sort(key=lambda k: k.stamp)
@@ -39,16 +54,14 @@ def _migrate(migrations: List[MigrationFile], conn: connection, schema: str = No
                 log.info(f'SKIP    {migration.stamp} {migration.name}')
                 stamp_match = True
         except ValueError:
-            log.warning(f'BADHASH {migration.stamp} {migration.name} Expected {current_mig.hash_} got {migration.hash_}')
+            log.warning(
+                f'BADHASH {migration.stamp} {migration.name} Expected {current_mig.hash_} got {migration.hash_}')
             stamp_match = True
 
         if stamp_match:
             current_mig = _next_or_none(prev_mig_iter)
         else:
-            curs = _get_schema_cursor(conn, schema)
-            _execute_file(migration, curs)
-            curs.close()
-            conn.commit()
+            await _execute_file(migration, conn)
 
 
 def _next_or_none(iterator):
@@ -58,17 +71,20 @@ def _next_or_none(iterator):
         return None
 
 
-def _execute_file(migration_file: MigrationFile, curs: cursor):
+async def _execute_file(migration_file: MigrationFile, conn: Connection):
     log.info(f'EXECUTE {migration_file.stamp} {migration_file.name}')
-    try:
-        with open(migration_file.path) as stream:
-            curs.execute(stream.read())
-    except psycopg2.Error as e:
-        log.error(e.pgerror)
-        raise e
 
-    t = """INSERT INTO arctic_tern_migrations VALUES (%s, %s, %s, now())"""
-    curs.execute(t, [migration_file.stamp, migration_file.name, migration_file.hash_])
+    try:
+        async with conn.transaction():
+            with open(migration_file.path) as stream:
+                migration_sql = stream.read()
+                await conn.execute(migration_sql)
+
+        insert_sql = "INSERT INTO arctic_tern_migrations VALUES ($1, $2, $3, now())"
+        await conn.execute(insert_sql, migration_file.stamp, migration_file.name, migration_file.hash_)
+    except PostgresError as e:
+        log.error(e)
+        raise e
 
 
 def _get_sql_files(dir: str) -> List[MigrationFile]:
@@ -82,7 +98,7 @@ def _get_sql_files(dir: str) -> List[MigrationFile]:
     return file_list
 
 
-def _prepare_meta_table(conn: connection, schema: str):
+async def _prepare_meta_table(conn: Connection, schema: str):
     create = f"""CREATE TABLE IF NOT EXISTS {schema or 'public'}.arctic_tern_migrations
                 (
                     stamp bigint NOT NULL PRIMARY KEY,
@@ -90,44 +106,20 @@ def _prepare_meta_table(conn: connection, schema: str):
                     sha3 char(56),
                     migrate_time timestamptz
                 );"""
-    with conn.cursor() as curs:  # type: cursor
-        curs.execute(create)
+    await conn.execute(create)
 
 
-def _fetch_previous_migrations(curs: cursor):
-    sql = """SELECT * FROM arctic_tern_migrations ORDER BY stamp asc"""
-    curs.execute(sql)
+async def _fetch_previous_migrations(conn: Connection):
+    sql = "SELECT * FROM arctic_tern_migrations ORDER BY stamp asc"
 
-    migrations = []
-    for row in curs:
-        stamp = row[0]
-        name = row[1]
-        sha3 = row[2]
-        mf = MigrationFile(stamp, name, None, sha3)
-        migrations.append(mf)
+    migrations = [
+        MigrationFile(
+            record["stamp"],
+            record["file_name"],
+            None,
+            record["sha3"]
+        )
+        for record in await conn.fetch(sql)
+    ]
 
     return migrations
-
-
-def _execute_with_schema(conn: connection, schema: str, *args, **kwargs):
-    with conn.cursor() as curs: # type: cursor
-        if schema:
-            curs.execute('SET search_path TO %s', [schema])
-        curs.execute(*args, **kwargs)
-
-
-def _get_schema_cursor(conn: connection, schema: str = None) -> cursor:
-    curs = conn.cursor()
-    if schema:
-        curs.execute('SET search_path TO %s', [schema])
-    return curs
-
-
-if __name__ == "__main__":
-    pub = psycopg2.connect(dbname='mig', user='postgres', password='root')
-    for f in _get_sql_files('../tests/scripts'):
-        print(f)
-    # migrate('../tests/scripts', conn=pub)
-    # tern = psycopg2.connect(dbname='mig', user='postgres', password='root', schema='tern')
-    # migrate('../tests/scripts', conn=pub, schema='tern')
-    # print(_get_sql_files('../tests/scripts'))
